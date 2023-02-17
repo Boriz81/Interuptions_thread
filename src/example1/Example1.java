b073e4587f04c609ed1dc518bcf2a76eba9f002f
Boriz81 <blitzboriz@gmail.com>
2023-02-17 20:31:22 +0900

    Initial commit

---
+++ src/example1/Example1.java
@@ -0,0 +1,11 @@
+package example1;
+
+public class Example1 {
+    public static void main(String[] args) {
+        Thread thread = new HelloThread1();
+        System.out.println(thread.isInterrupted());
+        thread.interrupt();
+        System.out.println(thread.isInterrupted());
+        
+    }
+}
